c8a8a40a039acaf43f93ce4fc46b0f978afd5eab
BugUser0815 <reisner.alex93@gmail.com>
2017-01-05 17:45:15 +0100

    Change dom0_program for live demo

--- host_dom0/dom0_program.py
+++ host_dom0/dom0_program.py
@@ -5,22 +5,34 @@ from dom0_sql import *
 import time
 
 session = Dom0_session('192.168.217.21', 3001)
+
+
+print('Waiting for 10 seconds...')
+i=0
+while i<100:
+	session.live()
+	time.sleep(0.1)
+	i=i+1
+
 session.read_tasks(script_dir + 'tasks.xml')
 session.send_descs()
 session.send_bins()
 session.start()
 
-print('Waiting for 10 seconds...')
-i=0
-while i<1000:
-	session.live()
-	time.sleep(0.01)
 
+while i<200:
+	session.live()
+	time.sleep(0.1)
+	i=i+1
 session.stop()
 
-while TRUE:
+
+while i<300:
 	session.live()
-	time.sleep(0.01)
+	time.sleep(0.1)
+	i=i+1
+
+
 
 session.profile(script_dir + 'log.xml')
 xml2sql(script_dir + 'log.xml', script_dir + 'dom0.db')
