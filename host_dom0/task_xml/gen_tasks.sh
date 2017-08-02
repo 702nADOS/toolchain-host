c1c117ea1230beb62ef4d7e559ac2f821e0b276a
Michael Zellner <zellnerm@in.tum.de>
2017-08-02 14:23:56 +0200

    New version

--- host_dom0/task_xml/gen_tasks.sh
+++ host_dom0/task_xml/gen_tasks.sh
@@ -5,7 +5,7 @@ name=pi
 for i in {0..100}
  do
    cp change_me_task.xml $name$i.xml
-   value1=$((42+$i*100)) 
+   value1=$((100+$i*$i*1000)) 
    value2=5M
    sed -i -e 's/changeme1/'$value1'/g' -e 's/changeme2/'$value2'/g' -e 's/changeme3/'$i'/g' $name$i.xml
 done
