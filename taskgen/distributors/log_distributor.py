12e72864ae9b4e9eb344c67d0ed95354b8f2808b
Johannes Fischer <johannes-fischer@mailbox.org>
2017-12-07 11:31:28 +0100

    colored print

--- taskgen/distributors/log_distributor.py
+++ taskgen/distributors/log_distributor.py
@@ -1,30 +1,63 @@
 import logging
+import time
 
 from taskgen.live import LiveResult
 from taskgen.taskset import TaskSet
 from taskgen.distributor import AbstractDistributor
 
-# stub
-import time
-    
+from pprint import pprint
+
+COUNTER = 0
+COLORS_FG = [
+    '\033[31m', # red
+    '\033[32m', # green
+    '\033[33m', # orange
+    '\033[34m', # blue
+    '\033[35m', # purple
+    '\033[36m', # cyan
+    '\033[37m', # lightgrey
+    '\033[90m', # darkgrey
+    '\033[91m', # lightred
+    '\033[92m', # lightgreen
+    '\033[93m', # yellow
+    '\033[94m', # lightblue
+    '\033[95m', # pink
+    '\033[96m' #lightcyan
+]
+COLOR_RESET = '\033[0m'
+
 class LogDistributor(AbstractDistributor):
     
     def __init__(self, host, port):
         self.logger = logging.getLogger("LogDistributor")
         self.logger.debug("stub created")
 
-    def start(self, optimization, taskset):
+        # select color
+        global COUNTER
+        self.index = COUNTER % len(COLORS_FG)
+        COUNTER = COUNTER + 1
+
+    def start(self, taskset, optimization):
         self.logger.debug("start of taskset")
         self._timestamp = time.clock()
+        self._print(taskset.asxml())
 
     def stop(self):
         self.logger.debug("stop of taskset")
 
     def live_request(self):
+        running = time.clock() - self._timestamp
         return LiveResult({
-            'running' : time.clock() - self._timestamp < 0.1 # 5 seconds
+            'running' : running < 0.1 # 1 seconds
         })
 
     def close(self):
         self.logger.debug("connection closed")
 
+    def _print(self,  obj):
+        print(COLORS_FG[self.index])
+        print(obj)
+        print(COLOR_RESET)
+
+
+        
