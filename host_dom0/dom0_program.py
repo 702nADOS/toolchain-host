c9bb87382662fcfbb0abd3dd466adb492aab5155
Alexander Reisner <reisner.alex93@gmail.com>
2018-01-16 12:50:51 +0100

    Adapt IP to new standard

--- host_dom0/dom0_program.py
+++ host_dom0/dom0_program.py
@@ -5,7 +5,7 @@ from dom0_sql import *
 import sys
 import time
 
-session = Dom0_session('192.168.218.21', 3001)
+session = Dom0_session('10.200.40.10', 3001)
 name = str(sys.argv[1])
 session.read_tasks(script_dir + name + '.xml')
 session.send_descs()
